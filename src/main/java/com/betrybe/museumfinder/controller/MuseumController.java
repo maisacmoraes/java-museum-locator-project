2b0cbd0c9825b95117b1232c0b2b9d1d5d55ea6c
maisamoraes <maisa.moraes@outlook.com>
2023-09-14 11:15:25 -0300

    05 - Rota POST /museums implementada

---
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -0,0 +1,39 @@
+package com.betrybe.museumfinder.controller;
+
+import static com.betrybe.museumfinder.util.ModelDtoConverter.dtoToModel;
+import static com.betrybe.museumfinder.util.ModelDtoConverter.modelToDto;
+
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/museums")
+public class MuseumController {
+  MuseumServiceInterface museumService;
+
+  @Autowired
+  public MuseumController(MuseumServiceInterface museumService) {
+    this.museumService = museumService;
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public MuseumDto createMuseum(@RequestBody MuseumDto museumDto) {
+    Museum museum = museumService.createMuseum(
+        dtoToModel(museumDto)
+    );
+
+    return modelToDto(museum);
+  }
+
+
+
+}
